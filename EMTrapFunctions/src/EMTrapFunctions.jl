cbeaa38807fa9dfa32b2b68c2bff2b3e3144f32f
derekaaf <derek.abrahamsf@gmail.com>
2024-11-30 20:13:25 -0500

    Final Version - Included potential and potential energy calculations for better interpretation - Added x-y plane view of the particle including potential countour plot and stream lines - Animations in test folder
    To do:
    - README
    - Animate using the new values for the particle's tail

--- EMTrapFunctions/src/EMTrapFunctions.jl
+++ EMTrapFunctions/src/EMTrapFunctions.jl
@@ -19,32 +19,61 @@ using Reexport
 @reexport using Plots
 @reexport using ProgressMeter
 @reexport using Printf
+@reexport using DataStructures
+@reexport using Colors
+@reexport using Profile
+@reexport using ProfileView
 
 # Export constants, functions, and types
-export μ₀, mₐ, qₐ, dt, lim, x, y, z, M, interval
+export μ₀, mₐ, qₐ, dt, lim, x, y, z, M, interval, simTime, Bmax, stepspersec
 export Dipole, NeuralNetwork
-export change_M!, bottleB, RK2step, Loss, Animate!, Train!, Simulate!
+export change_M!, bottleB, bottlePotential, RK2step, Loss, Animate!, Train!, test, Simulate
+export col, tailcolor
 
-const μ₀ = 4π * 10^-7
+const μ₀ = 4π * 10^-7 # N/A²
 const mₐ = 6.644657230e-27  # Example value for alpha particle mass in kg
 const qₐ = 3.2043533e-19  # Example value for alpha particle charge in C
-const dt = 5e-5
-const lim = 15
-const x = y = z = range(-lim, lim, length=100)
+const dt = 5e-5 # s
+const Bmax = 2e8 # A/m
+const lim = 15 # m
+const x = y = z = range(-lim, lim, length=100) 
 const M = Vec3f(0, 1, 0) # orientation of the dipole
-const simTime = 2 # s
+const simTime = 5 # s
 const interval = (v -> minimum(v) .. maximum(v))
+const stepspersec = Int(1/dt)
 
-mutable struct NeuralNetwork
+const col = to_color(:grey37)
+tailcolor = [RGBAf(col.r, col.g, col.b, (i/(stepspersec))^4) for i in 1:10*stepspersec]
+
+rng = Random.Xoshiro(2534)
+Random.seed!(rng)
+
+function Base.:+(a::Point3f, b::Tuple)
+    return a .+ b
+end
+
+Base.@kwdef mutable struct NeuralNetwork
     NN::Chain
+    type::String
     states::NamedTuple
     params::NamedTuple
-    optimizer
-    NeuralNetwork(NN, states, params, optimizer) = new(NN, states, params, optimizer)
-    NNCall(inputs) = NN(inputs, params, states)
-    updateNN(params, states) = (states = states, params = params)
-    updateOptimizer(∂params) = (optimizer, params = Optimisers.update(optimizer, params, ∂params))
-    () -> (NN; updateNN; updateOptimizer; NNCall)
+    optimizer::NamedTuple
+end
+
+function (NN::NeuralNetwork)(inputs, params)
+    return NN.NN(inputs, params, NN.states)
+end
+
+function (NN::NeuralNetwork)(inputs)
+    return NN.NN(inputs, NN.params, NN.states)
+end
+
+function test(NN::NeuralNetwork, inputs, states)
+    return NN.NN(inputs, NN.params, states)
+end
+
+function updateOptimizer!(NN::NeuralNetwork, ∂params)
+    (NN.optimizer, NN.params) = Optimisers.update(NN.optimizer, NN.params, ∂params)
 end
 
 mutable struct Dipole
@@ -52,7 +81,7 @@ mutable struct Dipole
     M::Vec3f
     m::Vec3f
     r::Point3f
-    Dipole(mag, m, r) = new(mag, mag * m, m, r)
+    Dipole(m, r) = new(0, Vec3f(0, 0, 0), m, r)
 end
 
 # Custom setter for the m field
@@ -62,16 +91,16 @@ function change_M!(dipole1::Dipole, dipole2::Dipole, new_mag::Float64)
     dipole2.M = dipole2.m * new_mag
 end
 
-Zygote.@adjoint Point{3, Float32}(x::AbstractArray) = Point{3, Float32}(x), Δ -> (nothing, Δ...)
-Zygote.@adjoint Vec{3, Float32}(x::AbstractArray) = Vec{3, Float32}(x), Δ -> (nothing, Δ...)
+Zygote.@adjoint Point{3, Float32}(x) = Point{3, Float32}(x), Δ -> (nothing, Δ...)
+Zygote.@adjoint Vec{3, Float32}(x) = Vec{3, Float32}(x), Δ -> (nothing, Δ...)
 
-function bottleB(r, M1::Dipole, M2::Dipole) :: Point3f
-    rA = r - M1.r
+function bottleB(r::Point3f, M1::Dipole, M2::Dipole) :: Point3f
+    rA = r .- M1.r
     rmagA = sqrt(dot(rA, rA))
     B1bottle = 3 * rA * dot(M1.M, rA) / rmagA^5
     B2bottle = -(M1.M) / rmagA^3
 
-    rB = r - M2.r
+    rB = r .- M2.r
     rmagB = sqrt(dot(rB, rB))
     B1bottle2 = 3 * rB * dot(M2.M, rB) / rmagB^5
     B2bottle2 = -(M2.M) / rmagB^3
@@ -81,15 +110,34 @@ function bottleB(r, M1::Dipole, M2::Dipole) :: Point3f
     return (μ₀ / (4 * π)) .* B_total
 end
 
+function bottlePotential(r::Point3f, M1::Dipole, M2::Dipole) :: Float64
+    rA = r .- M1.r
+    rmagA = sqrt(dot(rA, rA))
+    Potbottle1 = dot(M1.M, rA) / rmagA^3
+
+    rB = r .- M2.r
+    rmagB = sqrt(dot(rB, rB))
+    Potbottle2 = dot(M2.M, rB) / rmagB^3
+
+    Potential_total = .+(Potbottle1, Potbottle2)
+
+    return Potential_total/(4 * π)
+end
+
+function bottleU(r::Point3f, v::Vec3f, M1::Dipole, M2::Dipole) :: Float64
+    U = dot(cross(r, v), bottleB(r, M1, M2))
+    return -(qₐ/(2*mₐ)) * U
+end
+
 function RK2step(Bfield::Function, M1::Dipole, M2::Dipole, v::Vec3f, r::Point3f) :: Tuple{Point3f, Vec3f}
     dv1 = dt * (qₐ / mₐ) * cross(v, Bfield(r, M1, M2))
     dr1 = dt * v
 
-    dv2 = dt * (qₐ / mₐ) * cross(v + dv1, Bfield(r + dr1, M1, M2))
-    dr2 = dt * (v + dv1)
+    dv2 = dt * (qₐ / mₐ) * cross(v .+ dv1, Bfield(Point3f(r .+ dr1), M1, M2))
+    dr2 = dt * (v .+ dv1)
 
-    r_new = Point3f(r + (dr1 + dr2) / 2)
-    v_new = Vec3f(v + (dv1 + dv2) / 2)
+    r_new = Point3f(r .+ (dr1 .+ dr2) / 2)
+    v_new = Vec3f(v .+ (dv1 .+ dv2) / 2)
 
     # Check for NaN values
     if any(isnan, r_new) || any(isnan, v_new)
@@ -101,40 +149,66 @@ function RK2step(Bfield::Function, M1::Dipole, M2::Dipole, v::Vec3f, r::Point3f)
     return r_new, v_new
 end
 
-function Simulate!(NNₐ::NeuralNetwork, r::Point3f, v::Vec3f, v₀, magnitude::Float64, M1::Dipole, M2::Dipole) :: Tuple{Float64, Float64}
-    lossₐ = lossₐ = 0
+function Decisions!(r::Point3f, v::Vec3f, M1::Dipole, M2::Dipole, v₀::Vector{Float32}, magnitude₀::Float64, NNₐ::NeuralNetwork, paramsₐ::NamedTuple) :: Float64
+    loss = 0
     iter = 1
+    position_smoothness = 0
+    magnitude_smoothness = 0
+    energy_efficiency = 0
+    mag = magnitude₀
     while ((!any(x -> abs(x) >= lim, r)) && ((iter * dt) < simTime))
-        if (i % 10 == 0)
-            input = Float32.(vcat(r..., v..., M1.M..., M2.M...))
-            change_M!(M1, M2, NNₐ.NNCall(input)[1])
-        end
+        # if (iter * dt) % 1 == 0
+        input = Float32.(reshape(vcat(r..., v..., M1.M..., M2.M...), 12, 1))
+        mag, NNₐ.states = NNₐ(input, paramsₐ)
+        mag = mag[1]
+        # end
+        energy_efficiency += (mag)^2
+        magnitude_smoothness += norm(mag - M1.mag)
+        change_M!(M1, M2, mag)
         r₁, v = RK2step(bottleB, M1, M2, v, r)
-        lossₐ += norm(r₁ .- r) / iter
-        lossₚ += norm(r₁ .- r) / iter
+        position_smoothness += norm((r₁ .- r) / lim)
         r = r₁
         iter += 1
     end
-    lossₚ *= norm(v₀) + 1
-    lossₚ /= log10(magnitude + 2)
-    lossₚ *= exp(-(iter * dt))
-    lossₐ *= exp(-(iter * dt))
+    energy_efficiency /= (iter * Bmax^2)
+    magnitude_smoothness /= iter * Bmax
+    position_smoothness /= iter
+    loss = 0.5 * position_smoothness + 0.1 * magnitude_smoothness + 0.4 * energy_efficiency
+    loss *= exp(-(iter * dt))
 
-    return lossₚ, lossₐ
+    return loss
+end
+
+function Decisions!(r::Point3f, v::Vec3f, M1::Dipole, M2::Dipole, v₀::Vector{Float32}, magnitude₀::Float64, NNₐ::Nothing, paramsₐ::Nothing) :: Float64
+    loss = 0
+    iter = 1
+    position_smoothness = 0
+    while ((!any(x -> abs(x) >= lim, r)) && ((iter * dt) < simTime))
+        r₁, v = RK2step(bottleB, M1, M2, v, r)
+        position_smoothness += norm((r₁ .- r) / lim)
+        r = r₁
+        iter += 1
+    end
+    loss = position_smoothness/iter
+    loss *= norm(v₀) + 1
+    loss += (magnitude₀ / Bmax)^2
+    loss *= exp(-(iter * dt))
+
+    return loss
 end
 
 # Define the Loss function
-function Loss(NNₚ::NeuralNetwork, NNₐ::NeuralNetwork, M1::Dipole, M2::Dipole) :: Tuple{Float64, Float64, Float64}
-    inputs = Float32.(vcat(M1.M..., M2.M...))
-    output = NNₚ.NNCall(inputs)[1]
+function Loss(M1::Dipole, M2::Dipole, NNₚ::NeuralNetwork, paramsₚ::NamedTuple, NNₐ::Union{NeuralNetwork, Nothing} = nothing, paramsₐ::Union{NamedTuple, Nothing} = nothing) :: Tuple{Float64, Float64}
+    inputs = Float32.(vcat(M1.r..., M2.r..., M1.m..., M2.m...))
+    output, NNₚ.states = NNₚ(inputs, paramsₚ)
     r = Point3f(output[1])
     v = Vec3f(output[2])
     magnitude = Float64.(output[3][1])
-
     change_M!(M1, M2, magnitude)
-    lossₚ, lossₐ = Simulate!(NNₐ, r, v, output[2], magnitude, M1, M2)
     
-    return magnitude, lossₚ, lossₐ
+    loss = Decisions!(r, v, M1, M2, output[2], magnitude, NNₐ, paramsₐ)
+    
+    return magnitude, loss
 end
 
 # function Animate!(NN::Lux.Chain, sts::NamedTuple, params::NamedTuple, inputs, M1::Dipole, M2::Dipole)
@@ -183,33 +257,101 @@ end
 #     return Float64.(magnitude), loss, st
 # end
 
- function Train!(NNₚ::NeuralNetwork, NNₐ::NeuralNetwork, trainIt::Int, losshistory, paramshistory)
-    if (trainIt % 10 != 0) error("trainIt must be a multiple of 10") end
-    magnitude = rand(10e2:10e8)
-    M1 = Dipole(magnitude, M, [0.0, 10.0, 0.0]) # Dipole at (0, 10, 0)
-    M2 = Dipole(magnitude, M, [0.0, -10.0, 0.0]) # Dipole at (0, -10, 0)
-    progress = Progress(trainIt, 1, "Training Neural Networks")
-    for epoch in 1:trainIt
-        Fun = epoch % (0.1 * trainIt) == 0 ? Loss : Loss
-        (magnitude, lossₚ, lossₐ,), back = pullback(Fun, NNₚ, NNₐ, M1, M2)
+ function Train!(trainIt::Int, losshistory, fig, ax, label, NNₚ::NeuralNetwork, NNₐ::Union{NeuralNetwork, Nothing} = nothing)
+    # if (trainIt % 10 != 0) error("trainIt must be a multiple of 10") end
+    M1 = Dipole(M, [0.0, 10.0, 0.0]) # Dipole at (0, 10, 0)
+    M2 = Dipole(M, [0.0, -10.0, 0.0]) # Dipole at (0, -10, 0)
+    losses = Float64[]
 
-        _, ∂paramsₚ, ∂paramsₐ, _ = back((nothing, 1.0, 1.0))
-        NNₐ.updateOptimizer(∂paramsₐ)
-        NNₚ.updateOptimizer(∂paramsₚ)
-
-        push!(losshistory[1], lossₚ)
-        push!(losshistory[2], lossₐ)
-        change_M!(M1, M2, magnitude)
-
-        if epoch % (0.1 * trainIt) == 0
-            push!(paramshistory[1], NNₚ.params)
-            push!(paramshistory[2], NNₐ.params)
+    @showprogress dt=1 desc="Training Neural Network..." for epoch in 1:trainIt
+        if !isnothing(NNₐ)
+            (magnitude, loss,), back = pullback(Loss, M1, M2, NNₚ, NNₚ.params, NNₐ, NNₐ.params)
+            _, _, _, ∂paramsₚ, _, ∂paramsₐ = back((nothing, 1.0))
+            (isnothing(∂paramsₐ)) ? (@warn "∂paramsₐ is nothing at epoch $epoch") : nothing
+            updateOptimizer!(NNₐ, ∂paramsₐ);
+            NNₐ.states = Lux.update_state(NNₐ.states, :carry, nothing)
+        else
+            (magnitude, loss,), back = pullback(Loss, M1, M2, NNₚ, NNₚ.params)
+            _, _, _, ∂paramsₚ = back((nothing, 1.0))
+            isnothing(∂paramsₚ) ? (@warn "∂paramsₚ is nothing at epoch $epoch") : nothing
+            updateOptimizer!(NNₚ, ∂paramsₚ)
         end
 
-        next!(progress)
+        push!(losshistory, loss)
+        push!(losses, loss)
+        change_M!(M1, M2, magnitude)
+
     end
+
+    graph = lines!(ax, 1:trainIt, losses, color=:blue)
     GC.gc()
+    label.text[] = "Do you want to keep training?"
+    fig[2, 1] = buttongrid = Makie.GridLayout(tellwidth = false)
+    yesButton = buttongrid[1, 1] = Makie.Button(fig, label = "Yes")
+    noButton = buttongrid[1, 2] = Makie.Button(fig, label = "No")
     
+    click = Condition()
+    on(yesButton.clicks) do _
+        empty!(ax)
+        rm!(buttongrid, graph)
+        delete!(yesButton)
+        delete!(noButton)
+        label.text[] = ""
+        Train!(trainIt, losshistory, fig, ax, label, NNₚ, NNₐ)
+        notify(click)
+    end
+    on(noButton.clicks) do _
+        empty!(ax)
+        rm!(buttongrid, graph)
+        delete!(yesButton)
+        delete!(noButton)
+        label.text[] = ""
+        notify(click)
+    end
+
+    wait(click)
+    GC.gc()
+end
+
+function Simulate(animTime::Float64, NNₚ::NeuralNetwork, NNₐ::NeuralNetwork) :: Tuple{Vector{Point3f}, Vector{Float64}, Vector{Float64}}
+    r = Point3f[] 
+    magnitudes = Float64[]
+    U = Float64[]
+
+    M1 = Dipole(M, [0.0, 10.0, 0.0]) # Dipole at (0, 10, 0)
+    M2 = Dipole(M, [0.0, -10.0, 0.0]) # Dipole at (0, -10, 0)
+    initialValues = Float32.(vcat(M1.r..., M2.r..., M1.m..., M2.m...))
+    statesₚ = Lux.testmode(NNₚ.states)
+    (r₀, v, magnitude) = test(NNₚ, initialValues, statesₚ)[1]
+    change_M!(M1, M2, magnitude[1])
+    push!(magnitudes, magnitude[1])
+    push!(r, r₀)
+    u = bottleU(Point3f(r₀), Vec3f(v), M1, M2)
+    push!(U, u)
+    v = Vec3f(v)
+    i = 1 
+    progress = ProgressUnknown(desc="Calculating trajectories...", spinner = true)
+    statesₐ = Lux.testmode(NNₐ.states)
+    while ((!any(x -> abs(x) >= lim, r[i])) && ((i * dt) < animTime))
+        input = Float32.(reshape(vcat(r[i]..., v..., M1.M..., M2.M...), 12, 1))
+        mag, statesₐ = test(NNₐ, input, statesₐ)
+        push!(magnitudes, mag[1])
+        change_M!(M1, M2, mag[1])
+        r₁, v = RK2step(bottleB, M1, M2, v, r[i])
+        push!(r, r₁)
+        u = bottleU(r₁, v, M1, M2)
+        push!(U, u)
+        i += 1
+        next!(progress, spinner = "⌜⌝⌟⌞", showvalues = [("Time Trapped in seconds:", i * dt)])
+    end
+    println("Time Trapped = ", i * dt, "s")
+    GC.gc()
+    return (r, magnitudes, U)
+end
+
+function rm!(args...)
+    args = nothing
+    GC.gc()
 end
 
 end 
--- main.jl
+++ main.jl
@@ -2,88 +2,134 @@ using Pkg
 Pkg.activate("EMTrapFunctions")
 using EMTrapFunctions
 
-Page(listen_url = "localhost", listen_port = 9876)
+Page(listen_url = "localhost", listen_port = 2341)
 WGLMakie.activate!()
 Makie.inline!(true)
 
-rng = Random.Xoshiro(253467897)
+rng = Random.Xoshiro(262397)
+Random.seed!(rng)
 
-# Inputs: M, M. Output: x, y, z, vx, vx, vy, vz, magnitude
-NNₚ = Chain(
-    Dense(6 => 18, swish),
+fig = Figure(size = (1920, 1080))
+label = Label(fig[0, 1], justification = :center, fontsize = 40)
+ax = Makie.Axis(fig[1, 1], xlabel = "Iterations", ylabel = "Loss", aspect = 1, height = 600, width = 600)
+
+# Inputs: M, M. Output: x, y, z, vx, vx, vy, vz, dipole 1 position, dipole 2 position
+NN = Chain(
+    Dense(12 => 18, swish),
     Dense(18 => 31, relu),
     Dense(31 => 45, tanh),
     Dense(45 => 22),
     Parallel(nothing, Dense(22 => 6, sigmoid), Dense(22 => 6, softsign), Dense(22 => 13, x -> 10 * sigmoid(x))),
-    Parallel(nothing, Dense(6 => 3), Dense(6 => 6), Dense(13 => 8, x -> 10e5 * (sech(x))^2)),
+    Parallel(nothing, Dense(6 => 3), Dense(6 => 6), Dense(13 => 8, x -> 10e8 * (sech(x))^2)),
     Parallel(nothing, Dense(3 => 3, x -> lim/3 * tanh(x)), Dense(6 => 3, x -> 2 * lim * tanh(x)), Dense(8 => 1, abs))
 )
-params, sts = Lux.setup(rng, NNₚ)
-optimizer = Optimisers.setup(Adam(1e-5), params)
-NNₚ = NeuralNetwork(NNₚ, sts, params, optimizer)
+params, sts = Lux.setup(rng, NN)
+rules = OptimiserChain(ClipGrad(10), Adam(5e-4))
+optimizer = Optimisers.setup(rules, params)
+NNₚ = NeuralNetwork(NN, "Passive", sts, params, optimizer)
 
-NNₐ = Chain(
-    Dense(6 => 18, swish),
-) # Add more layers
-params, sts = Lux.setup(rng, NNₐ)
-optimizer = Optimisers.setup(RMSProp(1e-5), NNₐ.params)
-NNₐ = NeuralNetwork(NNₐ, sts, params, optimizer)
+# Inputs: x, y, z, vx, vy, vz, dipole moment 1, dipole moment 2. Output: magnitude
+NN = Chain(
+    GroupNorm(12, 4, affine = true),
+    Dense(12 => 12, swish),
+    StatefulRecurrentCell(LSTMCell(12 => 18, train_memory = true, train_state = true)),
+    Dense(18 => 31, relu),
+    Dropout(0.1),
+    Dense(31 => 45),
+    Dropout(0.2),
+    Dense(45 => 22, x -> 10 * sigmoid(x)),
+    Dropout(0.2),
+    Dense(22 => 1, x -> 1e2 * (sech(x))^2),
+    Dense(1 => 1, x -> clamp.(abs.(x * 1e6), 1e3, Bmax))
+)
+params, sts = Lux.setup(rng, NN)
+rules = OptimiserChain(ClipGrad(10), RMSProp(4e-3, 0.3))
+optimizer = Optimisers.setup(rules, params)
+NNₐ = NeuralNetwork(NN, "Active", sts, params, optimizer)
 
-losshistory = Tuple(Float64[])
-paramshistory = Tuple(typeof(params)[])
-TrainIT = 10
-Train!(NNₚ, NNₐ, TrainIT, losshistory, paramshistory)
-empty!(ax)
+
+losshistory = [Float64[] for _ in 1:2]
+# paramshistory = [typeof(NNₚ.params)[], typeof(NNₐ.params)[]]
+TrainIT = 200
+Train!(TrainIT, losshistory[1], fig, ax, label, NNₚ)
+Train!(TrainIT, losshistory[2], fig, ax, label, NNₚ, NNₐ)
 
 
-r = [Point3f[] for _ in 1:length(paramshistory)]
-for (R, param) in zip(r, paramshistory)
-    (r₀, v, magnitude) = NN(input, param, sts)[1]
-    change_M!(M1, M2, magnitude[1])
-    push!(R, r₀)
-    v = Vec3f(v)
-    i = 1
-    while ((!any(x -> abs(x) >= lim, R[i])) && ((i * dt) < 60))
-        r₁, v = RK2step(bottleB, M1, M2, v, R[i])
-        push!(R, r₁)
-        i += 1
-    end
-    println("Time Trapped = ", i * dt)
-end
-GC.gc()
+delete!(ax)
+delete!(label)
+# fig = Figure(resolution = (1080, 800))
+ax = LScene(fig[1, 1], show_axis=false, height = 800, width = 1000)
+ax2D = Makie.Axis(fig[1, 2][1, 1], aspect = 1, height = 300, width = 300)
+label = Label(fig[0, 1:2], justification = :center, fontsize = 40)
 
-fig = Figure(size=(1000, 1000))
-ax = LScene(fig[1, 1], show_axis=false, width = 800)
-label = Label(fig[0, 1], justification = :center, fontsize = 20)
+animTime = 60.0*60
+r, magnitudes, U = Simulate(animTime, NNₚ, NNₐ)
+M1 = Dipole(M, [0.0, 10.0, 0.0]) # Dipole at (0, 10, 0)
+M2 = Dipole(M, [0.0, -10.0, 0.0]) # Dipole at (0, -10, 0)
+change_M!(M1, M2, magnitudes[1])
 
-field = (r -> bottleB(r, M1, M2))
-streamplot!(ax, field, interval(x), interval(y), interval(z), density=0.3, alpha=0.1, transparency=true)
+field = (r -> bottleB(Point3f(r), M1, M2))
+streamplot!(ax, field, x, y, z, density=0.2, alpha=0.1, transparency=true)
 arrows!(ax, [M1.r, M2.r], [M1.M, M2.M], arrowsize=0.7, lengthscale= 1 / M1.mag)
-cam = Makie.Camera3D(ax.scene, projectiontype = Makie.Perspective)
 
-tracks = [Observable(Point3f[]) for _ in 1:length(r)]
-labels = [@sprintf("%.0f", (i/10) * TrainIT) for i in 1:length(r)]
-lines = []
+cam = Makie.Camera3D(ax.scene, projectiontype=Makie.Perspective)
+particle3D = Observable{Point3f}(r[1])
+particle2D = @lift(Point2f($particle3D[1], $particle3D[2]))
 
-plt = for (track, i, label) in zip(tracks, 1:length(r), labels)
-    push!(lines, lines!(ax, track, transparency=true, linestyle=:dash, colormap=:Paired_8))
-end
-legend = Legend(fig[1, 2], lines, labels, "Training Iterations", fontsize = 10)
+rₖ = @lift(Float64($particle3D[3]))
+field = @lift((r) -> Point2(bottleB(Point3f(r..., $rₖ), M1, M2)[1:2]))
+streamplot!(ax2D, field, x, y; density=1, alpha=0.5, transparency=true, depth_shift = 0.0)
+tail = CircularBuffer{Point3f}(10*stepspersec)
+fill!(tail, r[1])
+tail = Observable(tail)
+tail2D = @lift(Point2f.($tail))
 
-stop = Threads.Atomic{Bool}(true)
-i = Threads.Atomic{Int}(1)
-intvl = 50
-@sync while true
-    stop[] = true
-    @threads for j in 1:length(paramshistory)
-        if (i[]) <= length(r[j])
-            tracks[j][] = push!(tracks[j][], r[j][i[]])
-            # tracks[j][] = append!(tracks[j][], r[j][((i-1)*fps)+1:i*fps])
-            stop[] = false
+Makie.scatter!(ax, particle3D, markersize=10, color=:black)
+Makie.scatter!(ax2D, particle2D, markersize=10, color=:black)
+lines!(ax, @lift(collect($tail)); color=tailcolor, linewidth=5, transparency=true)
+lines!(ax2D, tail2D; color=tailcolor, linewidth=5, transparency=true)
+
+Base.size(screen::WGLMakie.Screen) = size(screen.scene)
+playback_duration = 10.0  # 60 seconds
+framerate = 30         # 30 FPS
+total_frames = playback_duration * framerate
+
+intval = (round(Int, length(r) / total_frames)) != 0 ? (round(Int, length(r) / total_frames)) : 1
+# Downsample indices
+frame_indices = 1:intval:length(r)
+
+potentials = Array{Float64}(undef, length(x), length(y), length(frame_indices))
+@showprogress dt=1 desc="Calculating Potentials..." @threads for k in eachindex(frame_indices)
+    for i in eachindex(x)
+        for j in eachindex(y)
+            potentials[i, j, k] = bottlePotential(Point3f(x[i], y[j], r[frame_indices[k]][3]), M1, M2)
         end
     end
-    label.text[] = @sprintf("Time Trapped = %.2f s", i[] * dt) 
-    # sleep(dt) # 1s * speed
-    Threads.atomic_add!(i, intvl)
-    if stop[] break end
 end
+
+Potential = Observable{Matrix{Float64}}(potentials[:, :, 1])
+Pot = Makie.contourf!(ax2D, x, y, Potential; transparency=true, colormap=(:RdBu, 0.5), depth_shift = 0.0)
+colbar = Colorbar(fig[1, 2][1, 2], Pot, label="Potential", labelpadding=20, width=20, ticklabelsize=10)
+
+# Precompute data for frames
+label_text = [@sprintf("Time Trapped = %.2f min. Magnitude of field = %.2f. \nPotential energy = %.2f", (i * dt) / 60, magnitudes[i], U[i]) for i in frame_indices]
+@profile record(fig, "EMTrap1.mp4", frame_indices; framerate=framerate) do idx
+    if idx == 1
+        zoom!(ax.scene, 0.6)
+    end
+    i = Int(ceil(idx/intval))
+    # Update track
+    previdx = max(idx - intval, 1)
+    particle3D[] = r[idx]
+
+    tail[] = append!(tail[], r[previdx:idx])
+    Potential[] = potentials[:, :, i]
+
+    # Update label
+    label.text[] = label_text[i]
+end
+
+delete!(ax)
+delete!(ax2D)
+delete!(label)
+delete!(colbar)
